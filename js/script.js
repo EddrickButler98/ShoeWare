bc7372f90ed0474e1e9ed63a52b284b2988e6cae
EddrickButler98 <butlereddrick@gmail.com>
2018-08-25 14:19:07 -0500

    New Store

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $("a").hover(function () {
+        $(this).css("background-color", "#20c997");
+    }, function () {
+        $(this).css("background-color", "#02B875");
+    });
+});
